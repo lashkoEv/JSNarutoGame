8e39784ce6b434d314425a898e3d3114d09ca944
Bob-development <proinvoke228@gmail.com>
2023-10-01 19:54:13 +0300

    css for form

--- apps/Form/Form.js
+++ apps/Form/Form.js
@@ -1,8 +1,9 @@
-import { AdvancedComponent } from "../core/Component";
-import { Input, Button } from "../components";
+import { AdvancedComponent } from "../../core/Component";
+import { Input, Button } from "../../components";
 
 export class Form extends AdvancedComponent {
   // returns new Auth|Reg Form
   constructor({ tagName, className, children, events, html, ...attrs }) {
-  } 
+
+  }
 }
--- main.js
+++ main.js
@@ -2,30 +2,8 @@ import { User, UserController, UserRepository } from "./schemas";
 import { Input } from "./components";
 import { render } from "./core/render";
 import { append } from "./core/append";
+import { Form } from "./apps/Form";
 
 import "./style.css";
 import { Button } from "./components/Button/Button";
 
-const user = new User({ login: "q", password: "q" });
-console.log(user);
-
-const userRepository = new UserRepository();
-console.log(userRepository);
-
-const userController = new UserController();
-console.log(userController);
-
-const input = new Input({
-  tagName: "input",
-  className: "input",
-  children: [],
-});
-
-const button = new Button({
-  tagName: "button",
-  className: "btn",
-  textContent: "",
-});
-
-render(app, input);
-append(app, button);
